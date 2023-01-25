c1d58ebaf41f0899406741d92469a7bdf041283f
Suraj Teke <surajkjteke@gmail.com>
2023-01-25 20:21:38 +0530

    adding java project

---
+++ SwapNumbers/src/com/acts/NumberSwapper.java
@@ -0,0 +1,12 @@
+package com.acts;
+
+public class NumberSwapper {
+
+	public static void main(String[] args) {
+		int a= 1;
+		int b= 0;
+		System.out.println("Swapped numbers are: " +b+", "+a);
+
+	}
+
+}
